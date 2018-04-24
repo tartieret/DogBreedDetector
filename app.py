1d3e4e93fd0a469efe62a82a19997742f08c6166
Thomas Tartiere <thomas.tartiere@panevo.com>
2018-04-23 17:39:38 -0700

    Add face detector

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
+from keras.applications.resnet50 import ResNet50, preprocess_input, decode_predictions
+from keras.preprocessing import image                  
+from tqdm import tqdm
 import os
-
+import numpy as np
 
 # CONFIGURATION
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -18,14 +21,69 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.secret_key = os.environ.get(
     'SECRET_KEY', 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT')  
 
-# generate urls for static files
+# --------------------------------------------------
+# Dog detector
+# -------------------------------------------------
+
+# define ResNet50 model
+ResNet50_model = ResNet50(weights='imagenet')
+
+def path_to_tensor(img_path):
+    # loads RGB image as PIL.Image.Image type
+    img = image.load_img(img_path, target_size=(224, 224))
+    # convert PIL.Image.Image type to 3D tensor with shape (224, 224, 3)
+    x = image.img_to_array(img)
+    # convert 3D tensor to 4D tensor with shape (1, 224, 224, 3) and return 4D tensor
+    return np.expand_dims(x, axis=0)
+
+def paths_to_tensor(img_paths):
+    list_of_tensors = [path_to_tensor(img_path) for img_path in tqdm(img_paths)]
+    return np.vstack(list_of_tensors)
+
+def ResNet50_predict_labels(img_path):
+    # returns prediction vector for image located at img_path
+    img = preprocess_input(path_to_tensor(img_path))
+    return np.argmax(ResNet50_model.predict(img))
+
+# returns "True" if a dog is detected in the image stored at img_path
+def dog_detector(img_path):
+    prediction = ResNet50_predict_labels(img_path)
+    return ((prediction <= 268) & (prediction >= 151))
+
+# --------------------------------------------------
+# Human detector
+# -------------------------------------------------
+
+# extract pre-trained face detector
+face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_alt.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_eye.xml')
+eye_glasses_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_eye_tree_eyeglasses.xml')
+
+def face_detector(img_path):
+    img = cv2.imread(img_path)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray)
+    
+    for (x,y,w,h) in faces:
+        # try to detect eyes
+        roi_gray = gray[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        # if no eyes, detected, look for glasses
+        if len(eyes)==0:
+            eyes_glasses = eye_glasses_cascade.detectMultiScale(roi_gray)
+            
+    
+    return (len(faces) > 0) and (len(eyes)>=1 or len(eyes_glasses)>=1)
+
+# --------------------------------------------------
+# Manage requests
+# -------------------------------------------------
 
 
 # Check if input file is allowed
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
 
 @app.route('/', methods=['GET', 'POST'])
 def homepage():
@@ -48,7 +106,7 @@ def homepage():
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
-            description = "This is a cat"
+            description = get_description(file)
 
         else:
             flash('Invalid file type', "error")
